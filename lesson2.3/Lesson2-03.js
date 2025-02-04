fc016244b2d8af6f92d082a3c923de0062ffb1f4
TonyFlyWebFlow <tonyfly2018@gmail.com>
2025-02-04 16:31:10 +0100

    Обновил код: теперь screenPrice вводится через prompt

--- lesson2.3/Lesson2-03.js
+++ lesson2.3/Lesson2-03.js
@@ -1,17 +1,47 @@
-const titleProject = 'NewProjekt3000';
-console.log(typeof titleProject);
+let titleProject = prompt('Название проекта');
+console.log("Название проекта:", titleProject);
 
-const screensValue = 'Шаблон, Уникум, Анимация';
-console.log(screensValue);
+let screensValue = prompt('Тип экрана: шаблонные, с уникальным дизайном, с анимациями');
+console.log("Выбранные экраны:", screensValue);
 
-const screenPrice = 1000;
-console.log(screenPrice);
+const screenPrice = +prompt('Цена за экраны');
+console.log("Цена за экраны:", screenPrice);
 
-const percentage = 10;
-console.log(percentage);
+let responsive = prompt('Нужен ли респонсивный сайт? (да/нет)') === 'да';
+console.log("Респонсивный сайт нужен:", responsive);
 
-const fullPrice = 2700;
-console.log(fullPrice);
+let service1 = prompt('Какой сервис нужен?');
+console.log("Сервис 1:", service1);
 
-let Responsive = true;
-console.log(Responsive);
+let servicePrice1 = prompt('Сколько это будет стоить?');
+console.log("Цена сервиса 1:", servicePrice1);
+
+let service2 = prompt('Какой еще сервис тебе нужен?');
+console.log("Сервис 2:", service2);
+
+let servicePrice2 = prompt('Сколько будет стоить второй сервис?');
+console.log("Цена сервиса 2:", servicePrice2);
+
+let fullPrice = screenPrice + +servicePrice1 + +servicePrice2;
+console.log("Полная стоимость проекта:", fullPrice);
+
+let discountPercentage; 
+
+
+if (fullPrice >= 0 && fullPrice < 20000) {
+    discountPercentage = 0;
+    console.log("Скидка не предусмотрена");
+} else if (fullPrice >= 20000 && fullPrice < 50000) {
+    discountPercentage = 5;
+    console.log("Скидка 5%");
+} else if (fullPrice >= 50000) {
+    discountPercentage = 10;
+    console.log("Скидка 10%");
+} else {
+    console.log("Что-то пошло не так");
+}
+
+let servicePercentPrice = fullPrice - (fullPrice * discountPercentage / 100);
+let roundedPrice = Math.round(servicePercentPrice);
+
+console.log("Итоговая сумма с вычетом процентов и скидки:", roundedPrice);
