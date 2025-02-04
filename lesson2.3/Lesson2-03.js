2d7ba05c2b90a7e59b44dba66a5da30c76a42250
TonyFlyWebFlow <tonyfly2018@gmail.com>
2025-02-04 12:00:32 +0100

    Добавил новые файлы в папку

---
+++ lesson2.3/Lesson2-03.js
@@ -0,0 +1,17 @@
+const titleProject = 'NewProjekt3000';
+console.log(typeof titleProject);
+
+const screensValue = 'Шаблон, Уникум, Анимация';
+console.log(screensValue);
+
+const screenPrice = 1000;
+console.log(screenPrice);
+
+const percentage = 10;
+console.log(percentage);
+
+const fullPrice = 2700;
+console.log(fullPrice);
+
+let Responsive = true;
+console.log(Responsive);
